299b2d5dadaca465e369cf625a2a4dbc4686f604
Lasse Siemoneit <lasse.siemoneit@gmail.com>
2023-11-21 01:24:09 +0100

    Added train.py file.

---
+++ src/train.py
@@ -0,0 +1,263 @@
+import chess
+import chess.svg
+import cairosvg
+import imgaug.augmenters as iaa
+import numpy as np
+from PIL import Image
+from sklearn.model_selection import train_test_split
+import os
+import random
+import shutil
+import logging
+from pathlib import Path
+from multiprocessing import Pool, Value
+import multiprocessing
+from tqdm import tqdm
+import io
+
+
+# Set a random seed for reproducibility
+np.random.seed(42)
+random.seed(42)
+
+data_dir = Path("data").resolve()
+
+
+def setup_logging(log_dir):
+    log_dir = Path(log_dir).resolve()
+    log_dir.mkdir(exist_ok=True)
+    log_file = log_dir / "app.log"
+
+    # Create a console handler with a higher log level
+    console_handler = logging.StreamHandler()
+    console_handler.setLevel(logging.INFO)
+
+    logging.basicConfig(
+        level=logging.INFO,
+        format="%(asctime)s [%(levelname)s] %(message)s",
+        handlers=[
+            logging.FileHandler(filename=log_file),
+            console_handler
+        ],
+    )
+    logging.info("Logging setup complete. Logs will be written to {}".format(log_file))
+
+
+# Sets up the train, test, and validation directories within the given data directory. Checks if directories already exist and contain data, warns if they do, creates them if needed. Logs status messages. Returns the data directory.
+def setup_directories(data_dir):
+    data_dir = Path(data_dir).resolve()
+    # Check if directories already exist and contain data
+    for dir_name in ["train", "test", "val"]:
+        dir_path = data_dir.joinpath(dir_name)
+        if dir_path.exists() and any(dir_path.iterdir()):
+            logging.warning(f"Directory {dir_path} already exists and contains data. Please clean up or use a new directory.")
+            return None
+        dir_path.mkdir(parents=True, exist_ok=True)
+
+    logging.info("Directories 'train', 'test' and 'val' created in {}".format(data_dir))
+    return data_dir
+
+
+# Augment images and log the process
+def augment_images(img, num_augmented_images):
+    aug = iaa.Sequential(
+        [
+            iaa.Affine(rotate=(-25, 25)),
+            # Removed flipping as it can alter the orientation of chess pieces
+            iaa.AdditiveGaussianNoise(scale=(10, 60)),
+            iaa.Crop(percent=(0, 0.2)),
+            iaa.LinearContrast((0.75, 1.5)),
+        ]
+    )
+
+    # Use multiprocessing to parallelize image augmentation
+    with Pool(multiprocessing.cpu_count()) as p:
+        augmented_images = list(
+            tqdm(
+                p.imap(aug.augment_image, [img] * num_augmented_images),
+                total=num_augmented_images,
+                desc="Augmenting images",
+            )
+        )
+
+    logging.info(f"Augmented {num_augmented_images} images.")
+    return np.array(augmented_images)
+
+
+
+# Generates a random chess position as a JPEG image file.
+def generate_position(i, data_dir):
+    data_dir = Path(data_dir).resolve()
+    output_file = None
+    try:
+        logging.info(f"Starting to generate position {i}")
+        board = chess.Board()
+        for _ in range(np.random.randint(1, 50)):
+            if not board.is_game_over():
+                move = random.choice(list(board.legal_moves))
+                board.push(move)
+
+        fen = board.fen()
+        # Validate FEN string
+        if not chess.Board(fen).is_valid():
+            logging.warning(f"Invalid FEN string generated for position {i}. Skipping.")
+            return None
+
+        safe_fen = fen.replace("/", "_")
+
+        output_file = data_dir / f"chess_position_{i}_{safe_fen}.jpg"  # Save as JPEG
+        if output_file.exists():
+            logging.warning(
+                f"Image {output_file} already exists. Skipping position {i}."
+            )
+            return None
+
+        # Convert board to SVG
+        svg_data = chess.svg.board(board=board)
+
+        # Directly convert SVG to JPEG
+        jpeg_data = cairosvg.svg2jpeg(bytestring=svg_data.encode("utf-8"))
+        logging.info(f"Converted SVG to JPEG for position {i}")
+
+        # Save JPEG to disk
+        with open(str(output_file), "wb") as f:
+            f.write(jpeg_data)
+        logging.info(f"Generated image for position {i} and saved to {output_file}")
+    except (chess.BoardError, cairosvg.Error, OSError) as e:
+        logging.error(f"Error occurred while generating position {i}: {type(e).__name__}, {e}")
+        # Ensure output_file is defined before attempting to check if it exists
+        if output_file and output_file.exists():
+            os.remove(output_file)
+            logging.info(f"Removed failed file {output_file}")
+        output_file = None
+    return output_file
+
+def generate_position_with_data_dir(i):
+    return generate_position(i, data_dir)
+
+
+def generate_images(num_positions, data_dir):
+    data_dir = Path(data_dir).resolve()
+    num_processes = min(multiprocessing.cpu_count(), num_positions)
+    counter = Value('i', 0)
+    with tqdm(total=num_positions, file=open(os.devnull, 'w')) as pbar:
+        with Pool(processes=num_processes) as pool:
+            for i, output_file in enumerate(pool.imap_unordered(generate_position_with_data_dir, range(num_positions))):
+                if output_file is not None:
+                    output_path = Path(output_file)
+                    if output_file.is_file():
+                        with output_path.open("rb") as f:
+                            with counter.get_lock():
+                                counter.value += 1
+                                pbar.update(counter.value)
+                            logging.info(f"Successfully generated image for position {i}")
+                    else:
+                            logging.warning(f"Failed to generate image for position {i}")
+            files = [f for f in pool.imap(generate_position_with_data_dir, range(num_positions)) if f is not None and Path(f).is_file()]
+    logging.info(f"Generated {len(files)} images out of {num_positions} requested")
+    return files
+
+# Split file paths and log the process
+def split_files(files, train_test_ratio, val_ratio):  # Added validation ratio
+    train_files, temp_files = train_test_split(
+        files, test_size=1 - train_test_ratio, random_state=42
+    )
+    val_files, test_files = train_test_split(
+        temp_files, test_size=val_ratio/(1-train_test_ratio), random_state=42
+    )  # Split remaining files into validation and test sets
+    logging.info(
+        f"Split files into {len(train_files)} training, {len(val_files)} validation and {len(test_files)} test files"
+    )
+    return train_files, val_files, test_files  # Return validation files
+
+
+def augment_and_save_images(files, image_size, num_augmented_images, data_dir, folder):
+    data_dir = Path(data_dir).resolve()
+    total_augmentations = len(files) * num_augmented_images
+    with tqdm(total=total_augmentations, desc="Augmenting images") as pbar:
+        for file_path in files:
+            aug_img_paths = []
+            if not os.path.exists(file_path):
+                logging.error(f"File {file_path} does not exist. Skipping.")
+                continue
+            try:
+                with Image.open(file_path) as img:
+                    img = np.array(img.resize(image_size))
+                    img = np.reshape(img, (image_size[0], image_size[1], 3))  # Reshape the image
+                    img = img.astype('uint8')  # Ensure the image is in the correct data type
+                    aug_imgs = augment_images(img, num_augmented_images)
+                    aug_img_paths = [
+                        data_dir.joinpath(folder, f"{file_path.stem}_{idx}.jpg")
+                        for idx in range(num_augmented_images)
+                    ]
+                    for aug_img, aug_img_path in zip(aug_imgs, aug_img_paths):
+                        with Image.fromarray(aug_img) as img:
+                            img.save(aug_img_path, "JPEG", quality=85)
+                logging.info(
+                    f"Augmented and saved {num_augmented_images} images for {file_path}"
+                )
+                pbar.update(num_augmented_images)  # Update the progress bar here
+            except Exception as e:
+                logging.error(
+                    f"Error occurred while augmenting and saving image {file_path}: {type(e).__name__}, {e}"
+                )
+                for aug_img_path in aug_img_paths:
+                    if aug_img_path.exists():
+                        os.remove(aug_img_path)
+                        logging.info(f"Removed failed file {aug_img_path}")
+
+
+
+# Move the files to the respective directories and log the process
+def move_files(files, data_dir, folder):
+    data_dir = Path(data_dir).resolve()
+    new_paths = []
+    for file_path in tqdm(files, desc=f"Moving files to {folder}", file=open(os.devnull, 'w')):
+        try:
+            new_path = data_dir.joinpath(folder, file_path.name)
+            shutil.move(str(file_path), str(new_path))
+            logging.info(f"Moved file {file_path} to {folder}")
+            new_paths.append(new_path)
+        except Exception as e:
+            logging.error(f"Error occurred while moving {folder} file {file_path}: {type(e).__name__}, {e}")
+    return new_paths
+
+
+def main():
+    log_dir = Path("log").resolve()
+    setup_logging(log_dir)
+    data_dir = setup_directories(Path("data").resolve())
+    if data_dir is None:  # Check if setup_directories returned None
+        logging.error("Data directory setup failed. Exiting.")
+        exit(1)
+    num_positions = 50
+    files = generate_images(num_positions, data_dir)
+
+    # Verify that the files were generated
+    if not files:
+        logging.error("No files generated. Exiting.")
+        exit(1)
+    train_test_ratio = 0.7  # Adjusted train-test ratio
+    val_ratio = 0.15  # Added validation ratio
+    train_files, val_files, test_files = split_files(files, train_test_ratio, val_ratio)  # Split into train, validation, and test sets
+    logging.info(f"{len(train_files)} training samples, {len(val_files)} validation samples, {len(test_files)} test samples")  # Log validation samples
+    image_size = (224, 224)
+    num_augmented_images = 5
+    # Move files into their respective directories and update paths
+    train_files = move_files(train_files, data_dir, "train")
+    val_files = move_files(val_files, data_dir, "val")
+    test_files = move_files(test_files, data_dir, "test")
+    # Augment images in their respective directories
+    augment_and_save_images(train_files, image_size, num_augmented_images, data_dir, "train")
+    augment_and_save_images(val_files, image_size, num_augmented_images, data_dir, "val")  # Augment validation images
+    augment_and_save_images(test_files, image_size, num_augmented_images, data_dir, "test")
+    logging.info("Training, validation, and test files are moved to the respective directories")  # Log validation directory
+    # Delete all files in the data directory after moving them into their respective sub directories
+    for file in data_dir.glob('*'):
+        if file.is_file():
+            file.unlink()
+    logging.info("All files in the data directory have been deleted after moving them into their respective sub directories")
+
+if __name__ == "__main__":
+    main()
+
